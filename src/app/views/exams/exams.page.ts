cc9ce36e11c026e34efa1f3127affedad983dde5
medmor <medmor@users.noreply.github.com>
2021-08-25 16:08:44 +0100

    download pdf

--- src/app/views/exams/exams.page.ts
+++ src/app/views/exams/exams.page.ts
@@ -9,13 +9,21 @@ export class ExamsPage {
 	public exams: string[];
 
 	constructor(public router: Router, public route: ActivatedRoute, public examsServer: ExamsServerService) {
-		this.exams = examsServer.getAllExams(this.route.snapshot.paramMap.get('branch'));
+		this.examsServer.branch = this.route.snapshot.paramMap.get('branch');
+		this.exams = examsServer.getAllExams(this.examsServer.branch);
+	}
+
+	public pdfHref(exam: string) {
+		return 'https://github.com/medmor/GitSvtImages/raw/main/docs/pdf/examens/'
+			+ this.examsServer.branch + '/'
+			+ this.parseExamName(exam) + '.pdf';
 	}
 
 	public navigateToExam(exam: string): void {
 		this.examsServer.examName = this.parseExamName(exam);
 		this.router.navigateByUrl('exam/' + this.examsServer.examName);
 	}
+
 	private parseExamName(exam: string): string {
 		const name = exam.split('–')
 			.map(a =>
