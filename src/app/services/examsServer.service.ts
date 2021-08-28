588b33356b86671eb9d7a5790f6a25eee2825f42
medmor <medmor@users.noreply.github.com>
2021-08-28 22:17:51 +0100

    ading exams 2021 rattrapage

--- src/app/services/examsServer.service.ts
+++ src/app/services/examsServer.service.ts
@@ -1,27 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { AllExams } from '../model/examsDammyDataBase.model';
+import { ALL_EXAMS } from '../model/examsDammyDataBase.model';
 import { Observable } from 'rxjs';
 import { CourseNodeModel } from '../model/courseNode.model';
 
 @Injectable({
-	providedIn: 'root',
+    providedIn: 'root',
 })
 export class ExamsServerService {
+    public readonly baseUrl = 'assets/exams/';
+    public exam: CourseNodeModel;
+    public branch = '';
+    public examName = '';
+    public answerModal: HTMLIonModalElement;
 
-	public readonly baseUrl = 'assets/exams/';
-	public exam: CourseNodeModel;
-	public branch = '';
-	public examName = '';
-	public answerModal: HTMLIonModalElement;
+    constructor(private http: HttpClient) {}
 
-	constructor(private http: HttpClient,) { }
-
-	public getAllExams(branch: string): string[] {
-		return AllExams[branch];
-	}
-	public getExam(): Observable<any> {
-		return this.http.get(this.baseUrl + this.branch + '/' + this.examName + '.json');
-	}
+    public getAllExams(branch: string): string[] {
+        return ALL_EXAMS[branch];
+    }
+    public getExam(): Observable<any> {
+        return this.http.get(
+            this.baseUrl + this.branch + '/' + this.examName + '.json'
+        );
+    }
 }
